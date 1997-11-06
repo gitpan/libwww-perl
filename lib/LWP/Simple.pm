cae91b7358be38fa1e7372d497275afad45d1fab
Gisle Aas <gisle@ActiveState.com>
1997-11-06 20:23:06 +0000

    Import of GAAS/libwww-perl-5.15 from CPAN.
    gitpan-cpan-distribution: libwww-perl
    gitpan-cpan-version:      5.15
    gitpan-cpan-path:         GAAS/libwww-perl-5.15.tar.gz
    gitpan-cpan-author:       GAAS
    gitpan-cpan-maturity:     released

--- lib/LWP/Simple.pm
+++ lib/LWP/Simple.pm
@@ -1,5 +1,5 @@
 #
-# $Id: Simple.pm,v 1.19 1996/07/23 19:29:24 aas Exp $
+# $Id: Simple.pm,v 1.24 1997/10/16 11:13:08 aas Exp $
 
 =head1 NAME
 
@@ -14,6 +14,7 @@ get, head, getprint, getstore, mirror - Procedural LWP interface
  if (mirror("http://www.sn.no/", "foo") == RC_NOT_MODIFIED) {
      ...
  }
+
  if (is_success(getprint("http://www.sn.no/"))) {
      ...
  }
@@ -21,38 +22,40 @@ get, head, getprint, getstore, mirror - Procedural LWP interface
 =head1 DESCRIPTION
 
 This interface is intended for those who want a simplified view of the
-libwww-perl library.  This interface should also be suitable for
-one-liners.  If you need more control or access to the header fields
-in the requests sent and responses received you should use the full OO
-interface provided by the LWP::UserAgent module.
+libwww-perl library.  It should also be suitable for one-liners.  If
+you need more control or access to the header fields in the requests
+sent and responses received you should use the full object oriented
+interface provided by the C<LWP::UserAgent> module.
 
-This following functions are provided (and exported) by this module:
+The following functions are provided (and exported) by this module:
 
 =over 3
 
 =item get($url)
 
-This function will get the document identified by the given URL.  The
-get() function will return the document if successful or 'undef' if it
-fails.  The $url argument can be either a simple string or a reference
-to a URI::URL object.
+The get() function will fetch the document identified by the given URL
+and return it.  It returns C<undef> if it fails.  The $url argument can
+be either a simple string or a reference to a URI::URL object.
 
 You will not be able to examine the response code or response headers
-(like I<Content-Type>) when you are accessing the web using this
-function.  If you need this you should use the full OO interface.
+(like 'Content-Type') when you are accessing the web using this
+function.  If you need this information you should use the full OO
+interface (see L<LWP::UserAgent>).
 
 =item head($url)
 
-Get document headers. Returns the following values if successful:
+Get document headers. Returns the following 5 values if successful:
 ($content_type, $document_length, $modified_time, $expires, $server)
 
-Returns an empty list if it fails.
+Returns an empty list if it fails.  In scalar context returns TRUE if
+successful.
 
 =item getprint($url)
 
-Get and print a document identified by a URL. The document is printet
-on STDOUT. The error message (formatted as HTML) is printed on STDERR
-if the request fails.  The return value is the HTTP response code.
+Get and print a document identified by a URL. The document is printed
+on STDOUT as data is received from the network.  If the request fails,
+then the status code and message is printed on STDERR.  The return
+value is the HTTP response code.
 
 =item getstore($url, $file)
 
@@ -67,9 +70,9 @@ the HTTP response code.
 
 =back
 
-This module also exports the HTTP::Status constants and
-procedures.  These can be used when you check the response code from
-getprint(), getstore() and mirror().  The constants are:
+This module also exports the HTTP::Status constants and procedures.
+These can be used when you check the response code from getprint(),
+getstore() and mirror().  The constants are:
 
    RC_CONTINUE
    RC_SWITCHING_PROTOCOLS
@@ -127,42 +130,74 @@ The module will also export the LWP::UserAgent object as C<$ua> if you
 ask for it explicitly.
 
 The user agent created by this module will identify itself as
-"LWP::Simple/0.00" and will initialize its proxy defaults from the
-environment (by calling $ua->env_proxy).
+"LWP::Simple/#.##" (where "#.##" is the libwww-perl version number)
+and will initialize its proxy defaults from the environment (by
+calling $ua->env_proxy).
 
 =head1 SEE ALSO
 
-L<LWP>, L<LWP::UserAgent>, L<HTTP::Status>, L<request>, L<mirror>
+L<LWP>, L<LWP::UserAgent>, L<HTTP::Status>, L<lwp-request>,
+L<lwp-mirror>
 
 =cut
 
 
 package LWP::Simple;
 
+use strict;
+use vars qw($ua %loop_check $FULL_LWP @EXPORT @EXPORT_OK $VERSION);
+
 require Exporter;
-@ISA = qw(Exporter);
-@EXPORT = qw(get head getprint getstore mirror);  # note additions below
+
+@EXPORT = qw(get head getprint getstore mirror);
 @EXPORT_OK = qw($ua);
 
-# We also export everything from HTTP::Status
+# I really hate this.  I was a bad idea to do it in the first place.
+# Wonder how to get rid of it???  (It even makes LWP::Simple 7% slower
+# for trivial tests)
 use HTTP::Status;
 push(@EXPORT, @HTTP::Status::EXPORT);
 
-require LWP;
-require LWP::UserAgent;
-use HTTP::Date qw(str2time);
-use Carp;
 
-$ua = new LWP::UserAgent;  # we create a global UserAgent object
-$ua->agent("LWP::Simple/$LWP::VERSION");
-$ua->env_proxy;
+$VERSION = sprintf("%d.%02d", q$Revision: 1.24 $ =~ /(\d+)\.(\d+)/);
+$FULL_LWP++ if grep {$_ eq "http_proxy"} keys %ENV;
+
+
+sub import
+{
+    my $pkg = shift;
+    my $callpkg = caller;
+    $FULL_LWP++ if grep $_ eq '$ua', @_;
+    Exporter::export($pkg, $callpkg, @_);
+}
+
+
+sub _init_ua
+{
+    require LWP;
+    require LWP::UserAgent;
+    require HTTP::Status;
+    require HTTP::Date;
+    $ua = new LWP::UserAgent;  # we create a global UserAgent object
+    my $ver = $LWP::VERSION = $LWP::VERSION;  # avoid warning
+    $ua->agent("LWP::Simple/$LWP::VERSION");
+    $ua->env_proxy;
+}
 
 
 sub get ($)
 {
-    my($url) = @_;
+    %loop_check = ();
+    goto \&_get;
+}
 
-    my $request = new HTTP::Request 'GET', $url;
+
+sub get_old ($)
+{
+    my($url) = @_;
+    _init_ua() unless $ua;
+
+    my $request = HTTP::Request->new(GET => $url);
     my $response = $ua->request($request);
 
     return $response->content if $response->is_success;
@@ -173,8 +208,9 @@ sub get ($)
 sub head ($)
 {
     my($url) = @_;
+    _init_ua() unless $ua;
 
-    my $request = new HTTP::Request HEAD => $url;
+    my $request = HTTP::Request->new(HEAD => $url);
     my $response = $ua->request($request);
 
     if ($response->is_success) {
@@ -185,23 +221,21 @@ sub head ($)
 		str2time($response->header('Expires')),
 		$response->header('Server'),
 	       );
-    } else {
-	return wantarray ? () : '';
     }
+    return;
 }
 
 
 sub getprint ($)
 {
     my($url) = @_;
+    _init_ua() unless $ua;
 
-    my $request = new HTTP::Request 'GET', $url;
-    my $response = $ua->request($request);
+    my $request = HTTP::Request->new(GET => $url);
     local($\) = ""; # ensure standard $OUTPUT_RECORD_SEPARATOR
-    if ($response->is_success) {
-	print $response->content;
-    } else {
-	print STDERR $response->error_as_HTML;
+    my $response = $ua->request($request, sub { print $_[0] });
+    unless ($response->is_success) {
+	print STDERR $response->status_line, " <URL:$url>\n";
     }
     $response->code;
 }
@@ -210,18 +244,83 @@ sub getprint ($)
 sub getstore ($$)
 {
     my($url, $file) = @_;
+    _init_ua() unless $ua;
 
-    my $request = new HTTP::Request 'GET', $url;
+    my $request = HTTP::Request->new(GET => $url);
     my $response = $ua->request($request, $file);
 
     $response->code;
 }
 
+
 sub mirror ($$)
 {
     my($url, $file) = @_;
+    _init_ua() unless $ua;
     my $response = $ua->mirror($url, $file);
     $response->code;
 }
+
+
+sub _get
+{
+    my $url = shift;
+    my $ret;
+    if (!$FULL_LWP && $url =~ m,^http://([^/:]+)(?::(\d+))?(/\S*)?$,) {
+	my $host = $1;
+	my $port = $2 || 80;
+	my $path = $3;
+	$path = "/" unless defined($path);
+	return _trivial_http_get($host, $port, $path);
+    } else {
+        _init_ua() unless $ua;
+	my $request = HTTP::Request->new(GET => $url);
+	my $response = $ua->request($request);
+	return $response->is_success ? $response->content : undef;
+    }
+}
+
+
+sub _trivial_http_get
+{
+   my($host, $port, $path) = @_;
+   #print "HOST=$host, PORT=$port, PATH=$path\n";
+
+   require IO::Socket;
+   local($^W) = 0;
+   my $sock = IO::Socket::INET->new(PeerAddr => $host,
+                                    PeerPort => $port,
+                                    Proto    => 'tcp',
+                                    Timeout  => 60) || return;
+   $sock->autoflush;
+   my $netloc = $host;
+   $netloc .= ":$port" if $port != 80;
+   print $sock join("\015\012" =>
+                    "GET $path HTTP/1.0",
+                    "Host: $netloc",
+                    "User-Agent: lwp-trivial/$VERSION",
+                    "", "");
+
+   my $buf = "";
+   my $n;
+   1 while $n = sysread($sock, $buf, 8*1024, length($buf));
+   return undef unless defined($n);
+
+   if ($buf =~ m,^HTTP/\d+\.\d+\s+(\d+)[^\012]*\012,) {
+       my $code = $1;
+       #print "CODE=$code\n";
+       if ($code =~ /^3/ && $buf =~ /\012Location:\s*(\S+)/) {
+           # redirect
+           my $url = $1;
+           return undef if $loop_check{$url}++;
+           return _get($url);
+       }
+       return undef unless $code =~ /^2/;
+       $buf =~ s/.+?\015?\012\015?\012//s;  # zap header
+   }
+
+   return $buf;
+}
+
 
 1;
