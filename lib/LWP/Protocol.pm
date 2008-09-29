a58cf19a9871babea771bde83db2557c6165becb
Gisle Aas <gisle@ActiveState.com>
2008-09-29 09:27:09 +0000

    Import of GAAS/libwww-perl-5.816 from CPAN.
    gitpan-cpan-distribution: libwww-perl
    gitpan-cpan-version:      5.816
    gitpan-cpan-path:         GAAS/libwww-perl-5.816.tar.gz
    gitpan-cpan-author:       GAAS
    gitpan-cpan-maturity:     released

--- lib/LWP/Protocol.pm
+++ lib/LWP/Protocol.pm
@@ -2,7 +2,7 @@ package LWP::Protocol;
 
 require LWP::MemberMixin;
 @ISA = qw(LWP::MemberMixin);
-$VERSION = "5.815";
+$VERSION = "5.816";
 
 use strict;
 use Carp ();
@@ -102,6 +102,7 @@ sub collect
         }
         elsif (!ref($arg) && length($arg)) {
             open(my $fh, ">", $arg) || die "Can't write to '$arg': $!";
+	    binmode($fh);
             push(@{$response->{handlers}{response_data}}, sub {
                 print $fh $_[3] || die "Can't write to '$arg': $!";
                 1;
